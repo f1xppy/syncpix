b9ebd975d456650ca844010351989414d6173790
f1xppy <aaf12102003@gmail.com>
2024-05-11 14:27:49 +0300

    restart

--- syncpixUI/app/(tabs)/_layout.tsx
+++ syncpixUI/app/(tabs)/_layout.tsx
@@ -1,35 +1,26 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+    <Tabs 
+      screenOptions={{ tabBarActiveTintColor: 'blue', 
+      headerShown: false,
+      tabBarStyle: { position: 'absolute' },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+          title: 'Photos',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="image" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="settings"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
         }}
       />
     </Tabs>
--- syncpixUI/app/(tabs)/settings.tsx
+++ syncpixUI/app/(tabs)/settings.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Tab() {
   return (
     <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Photos</Text>
+      <Text>Settings</Text>
     </View>
   );
 }
