7758319976a2d96586a702eb1ddf44c014e15f98
olegflax <108991212+olegflax@users.noreply.github.com>
2024-05-25 22:11:09 +0300

    Добавил гет запросик с выводом результата

--- syncpixUI/screens/test/Test.js
+++ syncpixUI/screens/test/Test.js
@@ -15,19 +15,29 @@ import {
 } from "react-native";
 import axios from 'axios';
 
-function heehee() {
-    const apiUrl='http://172.18.0.39:8000/devices?account_id=1';
-    axios.get(apiUrl).then(response);
-    return response;
-  }
+
+
 
 export default function Testik() {
+  const [text_content, setText] = useState('none');
+  
+  const getDevices = async() => {
+    const apiUrl='http://172.18.0.39:8000/devices?account_id=1';
+    try {
+      const response = await axios.get(apiUrl, {timeout: 2000});
+      setText(response.data);
+    } catch (err) {
+      setText(err.message);
+    }
+    //setText('Ttt');
+  }
   return (
     <View style={{flex: 1, justifyContent: "center", alignContent: "center"}}>
       <Button 
-        onPress={() => heehee}
+        onPress={getDevices}
         title="Нажми"
       />
+      <Text>{text_content}</Text>
     </View>
   );
 }
