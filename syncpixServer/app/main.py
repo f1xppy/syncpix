ef382364f84ab4be80394ec34ab03c8596df7366
f1xppy <aaf12102003@gmail.com>
2024-05-26 18:37:32 +0300

    Update main.py

--- syncpixServer/app/main.py
+++ syncpixServer/app/main.py
@@ -161,7 +161,7 @@ async def add_device(account_id: int, name: str, request: Request):
 
 
 @app.put("/devices/{id}", tags=["Devices"])
-async def update_device_address(id:int, request: Request):
+async def update_device_address(id: int, request: Request):
     device_to_update = session.query(Device).filter_by(id=id).first()
     if device_to_update:
         device_to_update.ip = request.client.host
@@ -175,7 +175,7 @@ async def update_device_address(id:int, request: Request):
 async def get_device_list(account_id: int):
     devices = session.query(Device).filter_by(account_id=account_id).all()
     if devices:
-        device_list = [int(device_id.__dict__["id"]) for device_id in devices]
+        device_list = [{"id":int(device_id.__dict__["id"]), "name":device_id.__dict__["name"]} for device_id in devices]
         return device_list
     else:
         return {"message": "account not found"}
